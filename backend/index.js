c63cb703e0747016578f36e1ec9597f929dae2be
itsathefak <athefayubkhan@gmail.com>
2024-09-22 13:47:42 -0400

    using get method

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/register", (req, res) => {
+  let { username, password } = req.query;
+  res.send(`Standard GET Response, Welcome ${username}`);
+});
+
+app.post("/register", (req, res) => {
+  res.send(`Standard POST Response, Welcome`);
+});
+
 app.listen(port, () => {
   console.log(`listening to port ${port} `);
 });
