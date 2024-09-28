8d6c3d03aa8c16b92b9df8987a932d5503494bf4
itsathefak <athefayubkhan@gmail.com>
2024-09-28 12:29:09 -0400

    Inheritance - extends and super

--- frontend/app.js
+++ frontend/app.js
@@ -1,44 +1,32 @@
-// Factory Functions
-// function personMaker(name, age) {
-//   const person = {
-//     name: name,
-//     age: age,
-//     talk() {
-//       console.log(`Hi, my name is ${this.name}, and my age is ${this.age}`);
-//     },
-//   };
-//   return person;
-// }
+//  Inheritance
 
-// let p1 = personMaker("Athef", 24);
-// let p2 = personMaker("Pandu", 25);
-
-// New Operators
-
-// Constructors
-
-// function Person(name, age) {
-//   this.name = name;
-//   this.age = age;
-// }
-
-// Person.prototype.talk = function () {
-//   console.log(`Hi, My Name is ${this.name}, and my age is ${this.age}`);
-// };
-
-// let p1 = new Person("Athiii", 24);
-// let p2 = new Person("Pandu", 25);
-
-// Using Classes - it always has a constructor inside it
-
+// Parent Class
 class Person {
   constructor(name, age) {
     (this.name = name), (this.age = age);
   }
   talk() {
-    console.log(`Hi, my Name is ${this.name}, and my age is ${this.age}`);
+    console.log(`Hi, My name is ${this.name}`);
   }
 }
 
-let p1 = new Person("Athiii", 24);
-let p2 = new Person("Pandu", 25);
+// Child class
+
+class Student extends Person {
+  //Student class is inheriting the functions and objects from parent class
+  constructor(name, age, marks) {
+    super(name, age); //Parent class constructor is being called
+    this.marks = marks;
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age, subject) {
+    super(name, age);
+    this.subject = subject;
+  }
+}
+
+let stu1 = new Student("Athiii", 24, 95);
+
+let tea1 = new Teacher("Pandu", 25, "GIS");
