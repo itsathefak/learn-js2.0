db06783766e921929191e760efd97a7b01ef4a94
itsathefak <athefayubkhan@gmail.com>
2024-09-28 11:50:25 -0400

    NEW operator

--- frontend/app.js
+++ frontend/app.js
@@ -1,14 +1,30 @@
 // Factory Functions
-function personMaker(name, age) {
-  const person = {
-    name: name,
-    age: age,
-    talk() {
-      console.log(`Hi, my name is ${this.name}, and my age is ${this.age}`);
-    },
-  };
-  return person;
+// function personMaker(name, age) {
+//   const person = {
+//     name: name,
+//     age: age,
+//     talk() {
+//       console.log(`Hi, my name is ${this.name}, and my age is ${this.age}`);
+//     },
+//   };
+//   return person;
+// }
+
+// let p1 = personMaker("Athef", 24);
+// let p2 = personMaker("Pandu", 25);
+
+// New Operators
+
+// Constructors
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-let p1 = personMaker("Athef", 24);
-let p2 = personMaker("Pandu", 25);
+Person.prototype.talk = function () {
+  console.log(`Hi, My Name is ${this.name}, and my age is ${this.age}`);
+};
+
+let p1 = new Person("Athiii", 24);
+let p2 = new Person("Pandu", 25);
